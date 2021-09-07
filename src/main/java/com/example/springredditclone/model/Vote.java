930660bc851dd963eeb61423965f2f102158c398
pratapnea <sereversion@gmail.com>
2021-09-07 12:17:59 +0545

    model created: Vote

--- src/main/java/com/example/springredditclone/model/Vote.java
+++ src/main/java/com/example/springredditclone/model/Vote.java
@@ -1,5 +1,35 @@
 package com.example.springredditclone.model;
 
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Builder
 public class Vote {
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long voteId;
+    private VoteType voteType;
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "postId", referencedColumnName = "postId")
+    private Post post;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "userId", referencedColumnName = "userId")
+    private User user;
 
 }
