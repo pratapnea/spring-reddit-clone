54ca67d7af6ad7a64eed60c10d359dcba7bc288d
pratapnea <sereversion@gmail.com>
2021-09-07 12:38:02 +0545

    model added: Comment

--- src/main/java/com/example/springredditclone/model/Comment.java
+++ src/main/java/com/example/springredditclone/model/Comment.java
@@ -1,5 +1,36 @@
 package com.example.springredditclone.model;
 
-public class Comment {
+import java.time.Instant;
 
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotEmpty;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Comment {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@NotEmpty
+	private String text;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "postId", referencedColumnName = "postId")
+	private Post post;
+	private Instant createdDate;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "userId", referencedColumnName = "userId")
+	private User user;
+	
 }
