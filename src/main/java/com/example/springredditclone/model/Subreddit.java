5e0e145c596a59de37384408210aaf069e5edc65
pratapnea <sereversion@gmail.com>
2021-09-07 11:14:51 +0545

    model created for: Subreddit

--- src/main/java/com/example/springredditclone/model/Subreddit.java
+++ src/main/java/com/example/springredditclone/model/Subreddit.java
@@ -1,5 +1,39 @@
 package com.example.springredditclone.model;
 
+import java.time.Instant;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotBlank;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Subreddit {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@NotBlank(message = "Community name is required")
+	private String name;
+	@NotBlank(message = "Description is needed")
+	private String description;
+	@OneToMany(fetch = FetchType.LAZY)
+	private List<Post> posts;
+	private Instant createdDate;
+	@ManyToOne(fetch = FetchType.LAZY)
+	private User user;
 
 }
