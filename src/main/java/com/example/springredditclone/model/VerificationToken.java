ae52d6434890929eaf2a9dd8de8b67ad79beddf4
pratapnea <sereversion@gmail.com>
2021-09-07 12:15:26 +0545

    model create: VerificationToken

--- src/main/java/com/example/springredditclone/model/VerificationToken.java
+++ src/main/java/com/example/springredditclone/model/VerificationToken.java
@@ -1,5 +1,32 @@
 package com.example.springredditclone.model;
 
-public class VerificationToken {
+import java.time.Instant;
 
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "token")
+public class VerificationToken {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String token;
+	@OneToOne(fetch = FetchType.LAZY)
+	private User user;
+	private Instant expiryDate;
+	
+	
 }
