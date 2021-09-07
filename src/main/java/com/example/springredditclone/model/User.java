6e4f0ff8c613ef01bfadb2b5857b7ad98078de76
pratapnea <sereversion@gmail.com>
2021-09-07 11:18:23 +0545

    model created for: User

--- src/main/java/com/example/springredditclone/model/User.java
+++ src/main/java/com/example/springredditclone/model/User.java
@@ -1,5 +1,36 @@
 package com.example.springredditclone.model;
 
+import java.time.Instant;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long userId;
+	@NotBlank(message = "Username is required")
+	private String userName;
+	@NotBlank(message = "Password is required")
+	private String password;
+	@Email
+	@NotBlank(message = "Email is required")
+	private String email;
+	private Instant createdDate;
+	private boolean enabled;
 
 }
